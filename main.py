5dd3b2e7458598444f114fc62314c8ca0d6dd227
Quinten <qvdpost@gmail.com>
2019-04-02 15:33:58 +0200

    The poets have been mysteriously silent on the subject of cheese. - GK Chesterton

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-# voeg de huidige structuur toe aan path
-import os, sys
-directory = os.path.dirname(os.path.realpath(__file__))
-sys.path.append(os.path.join(directory, "code"))
-sys.path.append(os.path.join(directory, "code", "classes"))
-sys.path.append(os.path.join(directory, "code", "algoritmes"))
-
 # importeer de gebruikte structuur
-from datastructuur import DataStructuur
+from code.classes import dataStructuur
 from bruteforce import randomize
 from hillclimber import hill_climber
 from breadthfirst import breadth_first
